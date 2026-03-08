fdeab6b44fc19ab4c4b96ea24f72508db9b3f2ca
ryoryoryoryouta-creator <ryoryoryoryouta@gmail.com>
2026-03-09 01:07:35 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "https://hbeslqwjmxtezlmuarth.supabase.co";
+export const SUPABASE_URL = "https://hbeslqwjnxtezlmuatrh.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_LxmIUppxIq4Q-PGDl8EpUg_tWMKOSCp";
